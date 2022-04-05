86875110cbdd773ad824cc13eeb8db03bbf4023f
raul-rauqoz <raul.quinonez@telus.com>
2022-04-05 13:38:34 -0600

    feat: mark

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override');
 
 const content = require('./content/cards.json');
 
-let arr_data = [ 'hard-coding' ];
+let arr_data = [ { task: 'hard-coding', mark: false } ];
 
 // Middlewares
 
@@ -25,37 +25,37 @@ app.set('view engine', 'ejs');
 // });
 
 app.get('/', (req, res) => {
-	res.status(200);
-	res.render('homework', { arr_data });
+	res.status(200).render('homework', { arr_data });
 });
 
 app.get('/add', (req, res) => {
-	res.status(200);
-	res.render('create');
+	res.status(200).render('create');
 });
 
 app.post('/create', (req, res) => {
-	arr_data.push(req.body.task);
-	res.status(200);
-	res.redirect('/');
+	arr_data.push({ task: req.body.task, mark: false });
+	res.status(200).redirect('/');
 });
 
 app.put('/update', (req, res) => {
-	arr_data[req.body.id] = req.body.task;
-	res.status(200);
-	res.redirect('/');
+	arr_data[req.body.id].task = req.body.task;
+	res.status(200).redirect('/');
 });
 
 app.get('/edit/:id', (req, res) => {
 	const id = req.params.id;
-	res.status(200);
-	res.render('edit', { task: arr_data[id], id });
+	res.status(200).render('edit', { task: arr_data[id], id });
+});
+
+app.get('/mark/:id', (req, res) => {
+	const id = req.params.id;
+	arr_data[id].mark = !arr_data[id].mark;
+	res.status(200).redirect('/');
 });
 
 app.delete('/delete/:id', (req, res) => {
 	arr_data.splice(req.params.id, 1);
-	res.status(200);
-	res.redirect('/');
+	res.status(200).redirect('/');
 });
 
 // Starting server.
